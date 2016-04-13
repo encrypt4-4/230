3b5e5d930345b4e8b6fff3e92d11453aff6bbfda
Ja'lon <jclark96@sbcglobal.net>
2016-04-13 17:55:35 -0500

    Added files via upload

---
+++ Assembler.java
@@ -0,0 +1,27 @@
+/*
+ * This program assembles Assembly code to binary/hex numbers and writes them to a MIP file to be used in Team 9's project
+ * 
+ * @file Assembler.java
+ * @author Ja'lon Clark, Val Hahn, Derek Vogel
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*;
+public class Assembler {
+	
+	//Converts inputs from decimal into binary
+	public int BinaryConverter(int val){
+		int binary = 0000;
+		
+		return binary;
+	}
+	//Converts inputs from binary into hexadecimal outputs
+	public String HexConverter(int val){
+		String hex = "0x00000";
+		
+		return hex;
+	}
+}
---
+++ AssemblerDriver.java
@@ -0,0 +1,15 @@
+/*
+ * Executes the Assembler class's methods.
+ * 
+ *  @file AssemblerDriver
+ *  @authors Ja'lon Clark, Val Hahn, Derek Vogel
+ *  @Version 1.0
+ */
+public class AssemblerDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
