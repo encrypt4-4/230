6aa4fd2bb65079a20833cc9b84bdbfdd2a25ac0b
Ja'lon <jclark96@sbcglobal.net>
2016-04-20 18:14:35 -0500

    Update Assembler.java

--- Assembler.java
+++ Assembler.java
@@ -18,9 +18,7 @@ public class Assembler {
 	//TODO create new exceptions to throw for invalid instructions
 	//Converts inputs from decimal into binary
 
-	private final Dictionary SINGLE_OP = new Hashtable();//dictionary for single ops
-	private final Dictionary DOUBLE_OP = new Hashtable();//dictionary for double ops
-	private final Dictionary JUMP_OP = new Hashtable();//dictionary for jumbs
+	private final Dictionary INSTRUCTION_OP = new Hashtable();//dictionary for single ops
 	private final Dictionary REGISTER_LOCATIONS = new Hashtable();//dictionary for registers
 	private final String PADDING = "0000000000";//for unused bits on single ops
 	private String opCode;//Instruction Op codes
@@ -35,33 +33,34 @@ public class Assembler {
 	private String type;//identifies eachi instruction type
 
 
-	public Assembler()
+	public Assembler(String[] inst)
 	{
 		this.opCode = " ";
 		this.registerCode=" ";
 		this.sBit = "0";
+		this.instruction = inst; //potential errors
 		/*TODO modify the tables to use strings and set conditions
 		 * using .contains to allow for flag setting using s bit.
 		 */
 
 		//Double Op instruction table
-		DOUBLE_OP.put("add", "00011");
-		DOUBLE_OP.put("sub", "00100");
-		DOUBLE_OP.put("and","00000");
-		DOUBLE_OP.put("or", "00001");
-		DOUBLE_OP.put("xor", "00010");
-		DOUBLE_OP.put("ld", "00101");
-		DOUBLE_OP.put("st", "00110");
+		INSTRUCTION_OP.put("add", "00011");
+		INSTRUCTION_OP.put("sub", "00100");
+		INSTRUCTION_OP.put("and","00000");
+		INSTRUCTION_OP.put("or", "00001");
+		INSTRUCTION_OP.put("xor", "00010");
+		INSTRUCTION_OP.put("ld", "00101");
+		INSTRUCTION_OP.put("st", "00110");
 
 		//Single Op instruction table
-		SINGLE_OP.put("dec", "01000");
-		SINGLE_OP.put("inc", "01001");
-		SINGLE_OP.put("jr", "01101");
-		SINGLE_OP.put("clr", "01100");
+		INSTRUCTION_OP.put("dec", "01000");
+		INSTRUCTION_OP.put("inc", "01001");
+		INSTRUCTION_OP.put("jr", "01101");
+		INSTRUCTION_OP.put("clr", "01100");
 
 		//Jump instruction table
-		SINGLE_OP.put("jeq", "10000");
-		SINGLE_OP.put("jl", "00001");
+		INSTRUCTION_OP.put("jeq", "10000");
+		INSTRUCTION_OP.put("jl", "00001");
 
 	}
 
@@ -80,7 +79,8 @@ public class Assembler {
 
 		switch(this.opCode.substring(0,2))
 		{
-		case "00":{
+		case "00":
+		{
 			this.type = "d";
 			break;
 		}
@@ -103,9 +103,12 @@ public class Assembler {
 
 	}
 	//takes the instruction input
-	public void setInstruction(String inst)
+	public void setInstruction()
 	{
-		switch()
+		switch(instruction[0])
+		{
+			
+		}
 		
 
 	}
